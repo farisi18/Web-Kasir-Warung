c87429f4f5c064617f800e0fcfb75cd407be7dea
farisi18 <jaka18fa@gmail.com>
2024-07-21 21:19:21 +0700

    Latihan Web Kasir UMKM Dengan PHP

---
+++ dashboard.php
@@ -0,0 +1,42 @@
+<?php
+include 'template/header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Pimpinan</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #DCDCDC; /* Ganti dengan warna yang diinginkan */
+        }
+    </style>
+</head>
+<h2>SELAMAT DATANG </h2>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card" style="width: 100%;">
+                <img src="assets/img/boy.jpg" class="card-img-top" alt="Profile Picture">
+                <div class="card-body">
+                    <h5 class="card-title">Pangestu Salman Farisi</h5>
+                    <p class="card-text">Junior Web Developer | AI enthusiast | Computer Science | Owner Buibu Mart</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card" style="width: 100%; background-color: rgba(0, 0, 0, 0.2);">
+                <div class="card-body">
+                    <h4 class="card-title">KAMU KAN ANAK IT</h4>
+                    <p class="card-text">Anak IT jurusan Ilkom (Informatika) adalah mereka yang memiliki minat dan passion dalam bidang teknologi informasi. Mereka adalah individu yang tertarik dengan pemrograman, analisis data, keamanan informasi, pengembangan aplikasi, dan topik terkait IT lainnya. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- Optional JavaScript -->
+<script type="text/javascript" src="js/jquery.slim.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
